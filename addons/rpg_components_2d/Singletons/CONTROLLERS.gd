70790b4d41ad4e5dbccff8d267bfc280d8d22538
M. Umar Shahbaz <68814294+MUmarShahbaz@users.noreply.github.com>
2026-06-05 09:47:08 +0500

    Updated File Structure

--- addons/rpg_components_2d/Singletons/CONTROLLERS.gd
+++ addons/rpg_components_2d/Singletons/CONTROLLERS.gd
@@ -1,5 +1,13 @@
 extends Node
+## This Singleton automatically creates a list off all connected controllers and automatically changes the light color of any newly connected controllers
 
+## List of connected controllers.[br]
+## [b]Note[/b]: [code]Controller[0][/code] is the default device (eg. KeyBoard/Mouse) and all others are gamepads.[br][br]
+## The dicionary contains:[br]
+## - [code]id[/code]: Device number ([code]-1[/code] means default device)[br]
+## - [code]name[/code]: Device name as a string[br]
+## - [code]has_light[/code]: Boolean for whether or not the controller has a light[br]
+## - [code]light[/code]: The color of the light ([code]0[/code] means there is no light, otherwise the value is of type [Color][br]
 var Controllers : Array[Dictionary] = [{
 	"id": -1,
 	"name": "Default",
@@ -28,6 +36,7 @@ func _joy_changed(device: int, connected: bool):
 		Controllers.remove_at(index)
 
 #region Light
+## Change a controller's light through here so that the [param Controllers] array stays updated.
 func change_light(device: int, color: Color):
 	Input.set_joy_light(device, color)
 	var index = Controllers.find_custom(func (x: Dictionary): return x.get("id") == device)
